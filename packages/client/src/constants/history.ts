9800b0087012445077ff95e0d5af4914ce98fc1d
Sebastian Bolanos <sebasheston@protonmail.com>
2021-03-04 22:37:40 +0000

    Move servers/relay to root of repo

--- packages/client/src/constants/history.ts
+++
@@ -1,7 +0,0 @@
-export const HISTORY_EVENTS = {
-  created: "record_created",
-  updated: "record_updated",
-  deleted: "record_deleted",
-};
-
-export const HISTORY_CONTEXT = "history";
--- packages/client/src/constants/subscription.ts
+++
@@ -1,6 +0,0 @@
-export const SUBSCRIPTION_EVENTS = {
-  payload: "subscription_payload",
-  created: "subscription_created",
-  updated: "subscription_updated",
-  deleted: "subscription_deleted",
-};
